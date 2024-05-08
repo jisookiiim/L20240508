b05c3d60aa967507ecf81271b4f99a0a974eb511
jisookim <jisookimwork@gmail.com>
2024-05-08 16:32:43 +0900

    counting star

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "**********" << endl;
+	cout << "**********" << endl;
+	cout << "**********" << endl;
+	cout << "&&________" << endl;
+
+	return 0;
+}
